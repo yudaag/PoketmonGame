16b63f4827697cdbe56449a9232b57ddfc7f846e
sw-develop <psw00719@naver.com>
2021-07-23 16:21:21 +0900

    [ADD] 포켓몬 추상 클래스 상속받은 클래스

---
+++ Poketmon/이상해씨.java
@@ -0,0 +1,27 @@
+package Poketmon;
+
+import java.util.ArrayList;
+
+import Weapon.Weapon;
+
+public class 이상해씨 extends Poketmon {
+
+    public 이상해씨(int power, int hp, String name){
+    	sethp(hp);
+    	setPower(power);
+    	this.name = name;
+    	weapons = new ArrayList<Weapon>(); //Weapon 타입 ArrayList 생성
+    	
+    	/*생성 확인*/
+    	System.out.println("[알림] "+ name + "이 생성되었습니다.");
+    }
+
+	@Override
+	public void attack() {
+		// TODO Auto-generated method stub
+		System.out.println("이상해씨 풀잎공격!!!");
+	}
+
+	
+
+}
